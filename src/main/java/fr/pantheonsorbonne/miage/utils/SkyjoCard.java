78a34f1456e8ba2afdbeafc35164504632b660ed
Cartier Lou <lou.cartier09@gmail.com>
2024-12-08 11:07:13 +0100

    git pull

---
+++ src/main/java/fr/pantheonsorbonne/miage/utils/SkyjoCard.java
@@ -0,0 +1,12 @@
+package fr.pantheonsorbonne.miage.utils;
+import fr.pantheonsorbonne.miage.utils.Valeur;
+
+public class SkyjoCard {
+
+    private Valeur valeur;
+
+    public SkyjoCard(Valeur valeur){
+        this.valeur = valeur;
+    }
+
+}
