93d93e019ad296d26208e8de343c252edad25dcf
Cybiaé Cornet <150437994+curdette@users.noreply.github.com>
2024-12-14 22:02:55 +0100

    Update Card.java

--- src/main/java/fr/pantheonsorbonne/miage/Skyjo/Card.java
+++ src/main/java/fr/pantheonsorbonne/miage/Skyjo/Card.java
@@ -1,23 +1,46 @@
 package fr.pantheonsorbonne.miage.Skyjo;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Card {
-    private final Value cardName;
+    protected final Value cardName;
+    private static HashMap<Value, Integer> nbCard = new HashMap<>();;
 
-    public Card(final Value cardName){
+    public Card(Value cardName) {
         this.cardName = cardName;
-    }    
 
-
-public Value getCardName(){
-    return this.cardName;
-}
-
-public Card[] getAllPossibleCards(){
-    Card[] allPossibleCards = new Card[150];
-    for(int i =0; i<150; i++){
-        for(Value cardName : Value.values()){
-            
-        }
     }
-}
+
+    public Value getCardName() { // surcharger dans hidden card FAIT
+        return this.cardName;
+    }
+
+    public static List<Card> getAllPossibleCards() { // utiliser une hashMap FAIT
+        nbCard.put(Value.MINUSTWO, 5);
+        nbCard.put(Value.ZERO, 15);
+        List<Card> possibleCards = new ArrayList<>(150);
+        for (Map.Entry<Value, Integer> entry : nbCard.entrySet()) {
+            for (int i = 0; i < entry.getValue(); i++) {
+                possibleCards.add(new Card(entry.getKey()));
+            }
+        }
+
+        for (Value values : Value.values()) {
+            if (values.getCardValue() != -2 && values.getCardValue() != 0 ) {
+                for (int i = 0; i < 10; i++) {
+                    possibleCards.add(new Card(values));
+                }
+            }
+        }
+        return possibleCards;
+    }
+
+    @Override
+    public String toString() {
+        return "" + this.getCardName().getCardValue();
+    }
+
 }
